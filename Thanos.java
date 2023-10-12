d88a6317df4b6e98f9ca18f08b460c3385cd97bb
KevinRoig <kevinroig@hotmail.com>
2023-10-12 15:19:24 +0200

    han, j'ai oublié le commit!

--- Thanos.java
+++ Thanos.java
@@ -4,24 +4,30 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+        Hero widow = new Hero("Black Widow", 34);
+        Hero captain = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero iron = new Hero("Iron Man", 48);
+        Hero scarlet = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero("Thor", 1500);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctor = new Hero("Doctor Strange", 42);
+        ArrayList<Hero> heroes = new ArrayList<>();
+        heroes.add(widow);
+        heroes.add(captain);
+        heroes.add(vision);
+        heroes.add(iron);
+        heroes.add(scarlet);
+        heroes.add(thor);
+        heroes.add(hulk);
+        heroes.add(doctor);
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+        thor.setAge(1501);
 
-        // TODO 3 : It's Thor birthday, now he's 1501
-
-        // TODO 4 : Shuffle the heroes list
-
-        // TODO 5 : Keep only the half of the list
-
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+        Collections.shuffle(heroes);
+        List<Hero> survivingHeroes = heroes.subList(0,4);
+        for(int i = 0; i < survivingHeroes.size(); i++){
+        System.out.println(survivingHeroes.get(i).getName());
+        }
     }
 }
